5dbde9711e973dc64d8c220c74eb9228f26b56d2
NikolaKasabov <n_kasabov@mail.bg>
2019-11-20 16:53:20 +0200

    some comments added

--- frontend-reactjs/src/components/register/RegisterForm-functional.js
+++ frontend-reactjs/src/components/register/RegisterForm-functional.js
@@ -20,6 +20,7 @@ const RegisterForm = (props) => {
   if (userData.password.length < 5) classNamePassword += ' warning';
   if (userData.repeatPassword !== userData.password || userData.repeatPassword.length < 5) classNameRepeatPassword += ' warning';
 
+  // on user typing
   const onInputChange = (ev) => {
     const inputName = ev.target.name;
     const inputValue = ev.target.value;
@@ -30,6 +31,7 @@ const RegisterForm = (props) => {
     });
   }
 
+  // on 'Register' button click
   const onFormSubmit = (ev) => {
     ev.preventDefault();
 
@@ -70,59 +72,5 @@ const RegisterForm = (props) => {
     </form>
   )
 }
-
-
-
-
-
-
-
-
-// class RegisterForm extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.onInputChange = this.onInputChange.bind(this);
-//     this.onFormSubmit = this.onFormSubmit.bind(this);
-
-//     this.state = {
-//       username: '',
-//       password: '',
-//     }
-//   }
-
-//   onInputChange(event) {
-//     const inputName = event.target.name;
-//     const inputValue = event.target.value;
-
-//     this.setState({ [inputName]: inputValue });
-//   }
-
-//   onFormSubmit(event) {
-//     event.preventDefault();
-
-//     fetch('http://localhost:5000/register', {
-//       method: 'POST',
-//       headers: { 'Content-Type': 'application/json' },
-//       body: JSON.stringify({
-//         username: this.state.username,
-//         password: this.state.password,
-//       }),
-//     })
-//   }
-
-//   render() {
-//     return (
-//       <form onSubmit={this.onFormSubmit}>
-//         <label htmlFor="register-username">Username: </label>
-//         <input type="text" id="register-username" name="username" onChange={this.onInputChange} />
-//         <br />
-//         <label htmlFor="register-password">Password: </label>
-//         <input type="password" id="register-password" name="password" onChange={this.onInputChange} />
-//         <br />
-//         <input type="submit" value="Register" />
-//       </form>
-//     )
-//   }
-// }
 
 export default RegisterForm;
